a765a1a7a0442b126b90d5ac77bfa38ccca44200
Zane Estere Gruntmane <141255754+esterzane@users.noreply.github.com>
2023-10-27 19:45:12 +0000

    domain generator smile submit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,20 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  // break
+
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let tld = [".org", ".com", ".pt", ".cat", ".dog"];
+
+  for (let value1 of pronoun) {
+    for (let value2 of adj) {
+      for (let value3 of noun) {
+        for (let value4 of tld) {
+          console.log(value1 + value2 + value3 + value4);
+        }
+      }
+    }
+  }
 };
